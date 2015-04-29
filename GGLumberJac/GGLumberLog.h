94c6e35e64b46600cea505e930368beeff6c5404
zero <osmk@qq.com>
2015-04-29 21:47:54 +0800

    build

---
+++ GGLumberJac/GGLumberLog.h
@@ -0,0 +1,39 @@
+//
+//  GGLumberLog.h
+//  __无邪_
+//
+//  Created by __无邪_ on 15/4/27.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+// CocoaLumberjac & XcodeColors(插件)
+// https://github.com/robbiehanson/XcodeColors  https://github.com/CocoaLumberjack/CocoaLumberjack
+// 在 EditScheme  Environment Variables 中添加 key: XcodeColors values:YES 可显示颜色
+// info.plist 中 Application supports iTunes file sharing 设置为 YES
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ *  @brief  在iTunes共享文档Logs目录下产出日志文件
+ *  @param
+ */
+
+/**
+ *  DDLogError(@"错误信息");    // 红色
+ *  DDLogWarn(@"警告");        // 橙色
+ *  DDLogInfo(@"提示信息");     // 默认是黑色
+ *  DDLogVerbose(@"详细信息");  // 默认是黑色
+ */
+
+/**
+ * 如果要修改Log输出的颜色可以使用如下代码：
+ * [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor blueColor] backgroundColor:nil forFlag:LOG_FLAG_INFO];
+ */
+
+@interface GGLumberLog : NSObject
++ (instancetype)sharedInstance;
+
+- (void)recordLog;
+- (void)redirectLogfile2ShareOfiTunes;
+@end
---
+++ GGLumberJac/GGLumberLog.m
@@ -0,0 +1,113 @@
+//
+//  GGLumberLog.m
+//  __无邪_
+//
+//  Created by __无邪_ on 15/4/27.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import "GGLumberLog.h"
+
+@interface GGLumberLog ()
+@property (strong, nonatomic) DDFileLogger *fileLogger;
+@end
+
+//static const int ddLogLevel = DDLogLevelVerbose;// 定义日志级别
+
+@implementation GGLumberLog
+
++ (instancetype)sharedInstance{
+    static GGLumberLog *logManager;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        logManager = [[GGLumberLog alloc] init];
+    });
+    return logManager;
+}
+
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        [self installLog];
+    }
+    return self;
+}
+
+
+
+#pragma mark - initialization
+
+- (void)installLog{
+    
+    
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+    
+    /*The code tells the application to keep a week's worth of log files on the system.*/
+    self.fileLogger = [[DDFileLogger alloc] init];
+    self.fileLogger.rollingFrequency = 60 * 60 * 24; // 24 hour rolling
+    self.fileLogger.logFileManager.maximumNumberOfLogFiles = 7;
+    
+    [DDLog addLogger:self.fileLogger];
+    
+    
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor yellowColor] backgroundColor:nil forFlag:DDLogFlagError];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor redColor] backgroundColor:nil forFlag:DDLogFlagWarning];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor blueColor] backgroundColor:nil forFlag:DDLogFlagInfo];
+    DDLogInfo(@"----------------------------------------------------------------------------------");
+    DDLogInfo(@"\n\n新的日志记录开始\n\n");
+    DDLogInfo(@"----------------------------------------------------------------------------------");
+}
+
+
+
+
+#pragma mark - public
+
+- (void)recordLog{
+};
+
+- (void)redirectLogfile2ShareOfiTunes{
+    
+    NSString *libPath     = [self dirCache];
+    NSString *logsPath    = [libPath stringByAppendingPathComponent:@"Logs"];
+    NSString *logName     = [[self.fileLogger currentLogFileInfo] fileName];
+    NSString *logOfSource = [logsPath stringByAppendingPathComponent:logName];
+    
+    NSString *docPath     = [self dirDocument];
+    NSString *subDocPath  = [docPath stringByAppendingPathComponent:@"Logs"];
+    
+    NSString *targetPath  = [subDocPath stringByAppendingPathComponent:logName];
+    
+    NSFileManager *fileM = [[NSFileManager alloc] init];
+    [fileM createDirectoryAtPath:subDocPath withIntermediateDirectories:YES attributes:nil error:nil];
+    if ([fileM fileExistsAtPath:targetPath]) {
+        [fileM removeItemAtPath:targetPath error:nil];
+    }
+    
+    NSError *err = nil;
+    [fileM copyItemAtPath:logOfSource toPath:targetPath error:&err];
+    
+    //    if (err) {
+    //        DDLogInfo(@"copy失败：%@",err);
+    //    }else{
+    //        DDLogInfo(@"copy成功");
+    //    }
+}
+
+
+
+- (NSString *)dirLibrary{
+    return NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES)[0];
+}
+
+- (NSString *)dirDocument{
+    return NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+}
+
+- (NSString *)dirCache{
+    return NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
+}
+@end
